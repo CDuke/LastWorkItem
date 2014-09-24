1111852ded07fcc3b65fdfcdb8d366bf98d1858e
Kulikov Denis <cdbiznes@gmail.com>
2014-09-24 14:22:02 +0400

    Add build.fsx

---
+++ build/build.fsx
@@ -0,0 +1,39 @@
+#r @"../tools/FAKE/tools/FakeLib.dll"
+open Fake
+open System.Xml
+
+// Properties
+let vsVersion = environVarOrDefault "VisualStudioVersion" "12.0"
+let buildDirBase = "./bin/"
+let buildDir = buildDirBase + vsVersion
+
+
+// files
+let slnReferences = !!"./src/LastWorkItem.sln"
+
+// Targets
+Target "Clean" (fun _ ->
+    CleanDir buildDir
+)
+
+Target "RestorePackages" (fun _ ->
+    !! "./**/packages.config"
+    |> Seq.iter (fun id -> (RestorePackage (fun p -> {p with OutputPath = "./lib"}) id))
+)
+
+Target "BuildApp" (fun _ ->
+    slnReferences
+        |> MSBuildRelease  buildDir "Build"
+        |> Log "AppBuild-Output: "
+)
+
+Target "All" DoNothing
+
+
+// Dependencies
+"Clean"
+    ==> "BuildApp"
+    ==> "All"
+
+// start build
+RunTargetOrDefault "All"
